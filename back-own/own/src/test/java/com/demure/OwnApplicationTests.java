2ddd2784a60aa5b91eaa536a22fec15419c9e47b
demure512 <130649482+demure512@users.noreply.github.com>
2023-07-19 09:36:24 +0800

    Add files via upload
    back

---
+++ back-own/own/src/test/java/com/demure/OwnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
